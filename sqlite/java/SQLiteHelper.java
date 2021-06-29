f1343f57bfe16f03d32e6d65c84964d34b258579
HAKStudio <53950869+HAKStudio@users.noreply.github.com>
2021-06-29 23:05:10 +0300

    Add files via upload

---
+++ sqlite/java/SQLiteHelper.java
@@ -0,0 +1,66 @@
+package com.sqlite;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SQLiteHelper extends SQLiteOpenHelper {
+    final String dbTable = "Users";
+    final String createDB = "CREATE TABLE " + dbTable + "(" +
+            "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+            "name TEXT," +
+            "surname TEXT" +
+            ")";
+
+    public SQLiteHelper(@Nullable Context context) {
+        super(context, "UsersDB", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(createDB);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + dbTable);
+        this.onCreate(db);
+    }
+
+    public void addUser(User user) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("name", user.getName());
+        values.put("surname", user.getSurname());
+        db.insert(dbTable, null, values);
+        db.close();
+    }
+
+    public ArrayList<User> getUsers() {
+        ArrayList<User> users = new ArrayList<>();
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM " + dbTable, null);
+        User user;
+
+        if (cursor.moveToFirst()) {
+            do {
+                user = new User();
+                user.setId(Integer.parseInt(cursor.getString(0)));
+                user.setName(cursor.getString(1));
+                user.setSurname(cursor.getString(2));
+                users.add(user);
+            } while (cursor.moveToNext());
+            return users;
+        } else {
+            return null;
+        }
+
+    }
+}
